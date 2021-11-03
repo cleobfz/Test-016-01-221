c777be57c814722490abe13d8633a8998fa424e0
user <user@192.168.103.142>
2021-11-03 12:07:25 +0100

    Eine Welli Klasse

--- GITTest/src/tiere/Welli.java
+++ GITTest/src/tiere/Welli.java
@@ -6,7 +6,23 @@ package tiere;
  *
  */
 public class Welli {
-/**
- * 
- */
+	
+	private String name;
+
+	public Welli(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Welli [name=" + name + "]";
+	}
 }

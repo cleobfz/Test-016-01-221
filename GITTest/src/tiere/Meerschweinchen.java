74df1dac5cbbe6066680f60dcaaa24eee02473bd
user <user@192.168.103.51>
2021-11-03 11:56:22 +0100

    Neue Klasse: Meerschweinchen

---
+++ GITTest/src/tiere/Meerschweinchen.java
@@ -0,0 +1,17 @@
+package tiere;
+
+public class Meerschweinchen {
+	
+	private String name;
+
+	public Meerschweinchen(String name) {
+		this.name = name;
+	}
+	public void geraeusche() {
+		
+	}
+	
+public static void main(String[] args) {
+	
+}
+}
